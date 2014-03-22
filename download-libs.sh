1405236759b751749638380f4796b492f643750f
Felix Hädicke <felixhaedicke@web.de>
2014-03-22 22:01:21 +0100

    Use curl instead of wget

--- download-libs.sh
+++ download-libs.sh
@@ -4,13 +4,13 @@ svn export http://llvm.org/svn/llvm-project/libcxxabi/trunk@201497 libcxxabi-rev
 tar czvf libcxxabi-rev201497.tar.gz libcxxabi-rev201497 || exit $?
 rm -rf libcxxabi-rev201497 || exit $?
 
-wget http://llvm.org/releases/3.4/libcxx-3.4.src.tar.gz || exit $?
-wget http://download.savannah.gnu.org/releases/freetype/freetype-2.4.12.tar.gz || exit $?
-wget http://sourceforge.net/projects/libjpeg-turbo/files/1.3.0/libjpeg-turbo-1.3.0.tar.gz || exit $?
-wget http://libsdl.org/release/SDL2-2.0.3.tar.gz || exit $?
-wget http://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz || exit $?
-wget http://www.libsdl.org/projects/SDL_net/release/SDL2_net-2.0.0.tar.gz || exit $?
-wget http://ftp.gnu.org/gnu/gettext/gettext-0.18.3.tar.gz || exit $?
-wget http://sourceforge.net/projects/libpng/files/zlib/1.2.8/zlib-1.2.8.tar.gz || exit $?
-wget http://downloads.sourceforge.net/project/libpng/libpng16/1.6.9/libpng-1.6.9.tar.gz || exit $?
+curl -L -O http://llvm.org/releases/3.4/libcxx-3.4.src.tar.gz || exit $?
+curl -L -O http://download.savannah.gnu.org/releases/freetype/freetype-2.4.12.tar.gz || exit $?
+curl -L -O http://sourceforge.net/projects/libjpeg-turbo/files/1.3.0/libjpeg-turbo-1.3.0.tar.gz || exit $?
+curl -L -O http://libsdl.org/release/SDL2-2.0.3.tar.gz || exit $?
+curl -L -O http://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.0.tar.gz || exit $?
+curl -L -O http://www.libsdl.org/projects/SDL_net/release/SDL2_net-2.0.0.tar.gz || exit $?
+curl -L -O http://ftp.gnu.org/gnu/gettext/gettext-0.18.3.tar.gz || exit $?
+curl -L -O http://sourceforge.net/projects/libpng/files/zlib/1.2.8/zlib-1.2.8.tar.gz || exit $?
+curl -L -O http://downloads.sourceforge.net/project/libpng/libpng16/1.6.9/libpng-1.6.9.tar.gz || exit $?
 
